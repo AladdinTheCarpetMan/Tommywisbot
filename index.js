45d382c6006494975a019c6c9f4d9955f300a577
Omer Aladdin <36205623+AladdinTheCarpetMan@users.noreply.github.com>
2018-03-03 22:29:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const Discord = require("discord.js");
 
 var bot = new Discord.Client();
@@ -33,12 +45,14 @@ console.log("You're my favourite customer!");
 bot.on("message", message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-    //if(message.content === (prefix + "")) {
-   // message.channel.send("")} else//
+    var args = message.content.substring(prefix.length).split(" ");
+    
+    switch (args[0].toLowerCase()) {
 
-    if(message.content.startsWith(prefix + "ping")) {
-       message.reply(`pong \`${Date.now() - message.createdTimestamp} ms\``);    
-    } else
+    case "ping":
+     message.reply(`pong \`${Date.now() - message.createdTimestamp} ms\``);    
+     break;
+    } 
 
     if(message.content.startsWith(prefix + "hello")) {
         message.reply("Oh hai Mark!");     
@@ -49,23 +63,23 @@ bot.on("message", message => {
      } else
 
      if(message.content === (prefix + "doubt")) {
-        message.reply("https://goo.gl/ti3Hmh");
+        message.reply("https://i.imgur.com/WNqO9Wd.jpg");
      } else
 
      if(message.content === (prefix + "betrayal")) {
-        message.channel.send("https://goo.gl/4EJgcN");
+        message.channel.send("https://i.imgur.com/q54x3XB.mp4");
      } else 
 
      if(message.content === (prefix + "hi")) {
-        message.channel.send("https://goo.gl/YqcwbD");
+        message.channel.send("https://i.imgur.com/92olrHD.gif");
      } else
 
      if(message.content === (prefix + "laugh")) {
-        message.channel.send("https://goo.gl/oMdWsy");
+        message.channel.send("https://i.imgur.com/P2fVH6S.jpg");
      } else
 
      if(message.content === (prefix + "mindblown")) {
-        message.channel.send("https://goo.gl/6MsSeh");
+        message.channel.send("https://i.imgur.com/UARUIAx.gif");
      } else
 
      if(message.content.startsWith(prefix + "sanic")) {
@@ -81,53 +95,129 @@ bot.on("message", message => {
      } else
 
      if(message.content === (prefix + "what a story")) {
-        message.channel.send("https://goo.gl/F5Dnc1");
+        message.channel.send("https://i.imgur.com/lNeQuMs.mp4");
     } else
-
+  
     if(message.content === (prefix + "basket")) {
-       message.channel.send("https://goo.gl/15VByX");
+       message.channel.send("https://imgur.com/dHr6stD");
     } else
 
     if(message.content === (prefix + "snakefu")) {
-       message.channel.send("https://goo.gl/aBrvoP");
+       message.channel.send("https://i.imgur.com/rUKZn2b.jpg");
     } else
 
     if(message.content === (prefix + "maidfu")) {
-       message.channel.send("https://goo.gl/t89iLw");
+       message.channel.send("https://i.imgur.com/Nu6KA21.jpg");
     } else
 
     if(message.content === (prefix + "squad")) {
-       message.channel.send("https://goo.gl/9wvDwD");
+       message.channel.send("https://imgur.com/PPqJsI0");
     } else
 
    if(message.content === (prefix + "starscream")) {
-    message.channel.send("https://goo.gl/1rcj8N");
+    message.channel.send("https://i.imgur.com/msJpfzf.jpg");
    } else
       
    if(message.content === (prefix + "stronk")) {
-      message.reply("https://goo.gl/FxPEXZ");
+      message.reply("https://i.imgur.com/xSmRfC4.png");
    } else
       
    if(message.content === (prefix + "ultrawaifu")) {
-      message.content("https://goo.gl/hGMB3L"); 
+      message.channel.send("https://i.imgur.com/IwZwQoH.jpg"); 
    } else
 
    if(message.content === (prefix + "franku")) {
-      message.channel.search("https://goo.gl/PYcFTk");
+      message.channel.send("https://i.imgur.com/d8g9TjA.jpg");
    } else
 
    if(message.content === (prefix + "cheep")) {
-      message.channel.send("https://goo.gl/y3Kj2p");
+      message.channel.send("https://i.imgur.com/YEAToLI.mp4");
    } else
 
    if(message.content === (prefix + "rage")) {
-      message.reply("https://goo.gl/SwV3Ys");
+      message.reply("https://i.imgur.com/APcWNZ9.mp4");
    } else
 
    if(message.content === (prefix + "satisfaction")) {
-      message.channel.send("https://goo.gl/hCbQbi");
-   }
-        
+      message.channel.send("https://i.imgur.com/c0BSEl7.png");
+   } else
+   
+   if(message.content === (prefix + "doggy")) {
+      message.channel.send("https://i.imgur.com/h7N8Mry.gif");    
+   } else
+   
+   if(message.content === (prefix + "hilarious")) {
+      message.channel.send("https://imgur.com/q5hP0hi");
+   } else
+   
+  if(message.content === (prefix + "noon")) {
+     message.channel.send("https://imgur.com/pqnbBYh");
+   } else
+  
+ if(message.content === (prefix + "sins")) {
+    message.channel.send("https://imgur.com/3Efpyzq");
+  } else
+  
+ if(message.content === (prefix + "chichi")) {
+    message.channel.send("https://i.imgur.com/qfHJnC3.png");
+ } else
+   
+if(message.content === (prefix + "admire")) {
+   message.channel.send("https://imgur.com/XV6EDzk");
+} else
+
+if(message.content === (prefix + "hooked")) {
+   message.channel.send("https://imgur.com/aOehKlK");
+} else
+  
+if(message.content === (prefix + "smile")) {
+  message.channel.send("https://imgur.com/RGQiV4C");  
+} else
+
+if(message.content === (prefix + "eagle")) {
+  message.channel.send("https://imgur.com/dFFNgrg");  
+} else
+
+if(message.content === (prefix + "pug")) {
+  message.channel.send("https://imgur.com/u3xGmjF");
+}
+
+if(message.content === (prefix + "knight")) {  
+  message.channel.send("https://imgur.com/JOQoJWJ");
+} else
+  
+if(message.content === (prefix + "mitch")) {
+  message.channel.send("https://imgur.com/7AJ3Qf1");
+} else
+  
+if(message.content === (prefix + "look")) {
+  message.channel.send("https://imgur.com/f1T9xnJ");
+} else
+
+if(message.content === (prefix + "overthere")) {
+  message.channel.send("https://imgur.com/Rfjw5qe");
+} else
+  
+if(message.content === (prefix + "overhere")) {
+  message.channel.send("https://imgur.com/c6N73Bs");  
+} else
+
+if(message.content === (prefix + "change")) {
+   message.channel.send("https://imgur.com/xm8XFvs");
+}
+  
+if(message.content === (prefix + "wish")) {
+   message.channel.send("https://imgur.com/klz4Gy9");
+} else
+
+if(message.content === (prefix + "ride")) {
+  message.channel.send("https://imgur.com/pSdMQt0");
+} else
+
+if(message.content === (prefix + "cosby")) {
+  message.channel.send("https://imgur.com/SQ4f4bU");
+}
+  
 });
 
-bot.login(token);
+bot.login(process.env.TOKEN);
